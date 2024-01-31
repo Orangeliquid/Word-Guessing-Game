8499828f6bf00bfb7af09410a1dc1843662faa38
Orangeliquid <hardym2020111@gmail.com>
2024-01-31 01:17:44 -0500

    Make a function for the whole game and utilize if __name__ convention

--- main.py
+++ main.py
@@ -1,43 +1,44 @@
 import random
 
-# Feel Free to add your own words or even a separate file to pull words from
-word_list = ["apple", "peach", "banana", "pear", "orange"]
-word_selected = random.choice(word_list)
-word_blanks = ["_" for i in word_selected]
 
-user_guesses = []
-guesses = 0
+def guessing_game():
+    # Feel free to add words to the list or pull words from a file (words.txt)
+    word_list = ["apple", "peach", "banana", "pear", "orange"]
+    word_selected = random.choice(word_list).lower()
+    word_blanks = ["_" for _ in word_selected]
 
-while True:
-    if "_" not in word_blanks:
-        print("\n" * 30)
-        print(f"Congrats, you found the word -> {word_selected}")
-        print(f"You used {guesses} guesses")
-        break
+    user_guesses = []
+    guesses = 0
 
-    print(word_blanks)
-    guessed_character = input("\nTake a guess at what the word is. Use one character at a time: ").lower()
-    if guessed_character == "quit":
-        break
+    while True:
+        if "_" not in word_blanks:
+            print("\n" * 30)
+            print(f"Congrats, you found the word -> {word_selected}")
+            print(f"You used {guesses} guesses")
+            break
 
-    # making sure input is at least a character
-    if len(guessed_character) != 0:
+        print(" ".join(word_blanks))
+        guessed_character = input("\nTake a guess at what the word is. Use one character at a time: ").lower()
+
+        if guessed_character == "quit":
+            print(f"The word was {word_selected}. Thanks for playing!")
+            break
+
+        # Validate user input
+        if not guessed_character.isalpha():
+            print("Please enter a valid alphabetical character")
+            continue
+
         guesses += 1
 
-        if guessed_character in [i for i in word_selected]:
-
-            # looping through all words in word_list
-            for char in word_selected:
-                # lopping through all characters in word from word_list
+        if guessed_character in word_selected:
+            # Replace blanks with guessed character
+            for i, char in enumerate(word_selected):
                 if guessed_character == char:
-                    # Grabbing the index(es) of the character guess in the word_selected
-                    char_enum = [index for (index, item) in enumerate(word_selected) if item == guessed_character]
-
-                    # Iterating through char_enum index(es) within word_selected and replacing blank with guessed char
-                    for i in char_enum:
-                        word_blanks[i] = guessed_character
+                    word_blanks[i] = guessed_character
         else:
-            print(f"{guessed_character} is not in word")
+            print(f"{guessed_character} is not in the word")
 
-    else:
-        print("Please enter a character")
+
+if __name__ == "__main__":
+    guessing_game()
